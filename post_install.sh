f5a359c7975652c7d889aa1ef50e9b544f8eccdd
Richard <richard2891@gmail.com>
2022-02-09 23:08:08 -0500

    update to stop using root user and start useing btfs user. fix white spaces.

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -x
 IP_ADDRESS=$(ifconfig | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}')
 
+USER_PASS=$(openssl rand -base64 20 | md5 | head -c6)
+
 fetch https://github.com/SumGuyV5/iocage-plugin-btfs/releases/download/1.6.0/go-btfs-1.6.0.pkg
 
 pkg install -y go-btfs-1.6.0.pkg
@@ -8,18 +10,25 @@ rm go-btfs-1.6.0.pkg
 
 sysrc btfs_enable=YES
 
-INIT=$(btfs init)
+mkdir -p /usr/home/btfs
+ln -s /usr/home /home
 
-btfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["http://'"${IP_ADDRESS}"':5001", "http://0.0.0.0:5001"]'
-btfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
+pw user add btfs -c btfs -s /bin/sh
+echo $USER_PASS | pw usermod -n btfs -h 0
+chown -R btfs:btfs /home/btfs
 
-btfs config --json Addresses.API '"/ip4/'"${IP_ADDRESS}"'/tcp/5001"'
+mkdir -p /var/run/btfs
+chown -R btfs:btfs /var/run/btfs/
 
-btfs config profile apply storage-host
+su -l btfs -c "btfs init > btfs_info"
+su -l btfs -c "btfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '[\"http://'"${IP_ADDRESS}"':5001\", \"http://0.0.0.0:5001\"]'"
+su -l btfs -c "btfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '[\"PUT\", \"GET\", \"POST\"]'"
+su -l btfs -c "btfs config --json Addresses.API '\"/ip4/'"${IP_ADDRESS}"'/tcp/5001\"'"
+su -l btfs -c "btfs config profile apply storage-host"
 
 service btfs start
 
-echo -e "${INIT}.\n"  > /root/PLUGIN_INFO
+echo -e "$(cat /home/btfs/btfs_info).\n" > /root/PLUGIN_INFO
 echo -e "btfs now installed.\n" >> /root/PLUGIN_INFO
 echo -e "\nPlease open your web browser and go to http://${IP_ADDRESS}:5001/hostui to configure btfs.\n" >> /root/PLUGIN_INFO
 echo -e "\nYou can also go to http://${IP_ADDRESS}:5001/webui \n" >> /root/PLUGIN_INFO
